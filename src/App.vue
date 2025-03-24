2362262ded5a2f2708751a68f7cb440ffc7a3c61
spahicadis <adis@spahica.com>
2025-03-24 19:24:46 +0100

    First commit, i made basic layout next will be responsivnes and toggle for dark mode

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <div class="h-[100vh] bg-linear-to-b from-emerald-950 to-indigo-950">
+    <h1 class="text-center text-6xl p-10">
+      Pozdrav, ja sam <span class="text-cyan-400">Adis Spahić</span>
+    </h1>
+    <hr class="w-7xl border-2 opacity-75 rounded-[9999px] mx-auto" />
+    <div class="container mx-auto">
+      <h3 class="mt-5 text-2xl text-teal-100/75">Moji omiljeni filmovi</h3>
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="w-[320px] h-[480px] pt-5">
+          <img
+            class="object-fit rounded-md"
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSUX_2arZYl22hAX0hY19pOhDk0XyYtODSkWYQ_m0GtUq5ACocX"
+          />
+          <h2 class="text-center text-2xl mt-2">Spašavanje vojnika Ryana</h2>
+        </div>
+        <div class="w-[320px] h-[480px] pt-5">
+          <img
+            class="object-fit rounded-md"
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQmUZve5jOATMSYj-wqlh_5Jca446l83msaDJgwwi8KCeZQkzrI"
+          />
+          <h2 class="text-center text-2xl mt-2">Midway</h2>
+        </div>
+        <div class="w-[320px] h-[480px] pt-5">
+          <img
+            class="object-fit rounded-md"
+            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ7PRcrPXSy_xYkOl26n2C9LxEPos-53ECogYvBKWIAjq6Ank38"
+          />
+          <h2 class="text-center text-2xl mt-2">Smisao života jednoga psa</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
